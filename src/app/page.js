3230991d3751702692b8945add5c75df49575f86
Kevin Lee <hlee6997@uni.sydney.edu.au>
2024-09-27 17:14:34 +1000

    Initial Deployment Attempt 2

--- src/app/page.js
+++ src/app/page.js
@@ -10,29 +10,15 @@ export default function Home() {
   const [user] = useAuthState(auth);
   const [userSession, setUserSession] = useState(null);
   const router = useRouter();
-
-  // Use useEffect to safely access sessionStorage in the client
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const storedUser = sessionStorage.getItem('user');
-      setUserSession(storedUser);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!user && !userSession) {
-      router.push('/signUp');
-    }
-  }, [user, userSession, router]);
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={() => {
+      {/* <button onClick={() => {
         signOut(auth)
         sessionStorage.removeItem('user')
         }}>
         Log out
-      </button>
+      </button> */}
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
